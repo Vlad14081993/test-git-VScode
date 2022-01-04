d36d585573285ca7cc53e3888a3a3cf61106adfb
Vlad <vladsuroviy3@gmail.com>
2022-01-04 22:45:35 +0200

    Загрузка барахла просто так

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
-"use strict";
+'use strict';
 
-  
+
+
+
+// Video 020 Объекты деструктуризация объектов
+
+const options = {
+    name: 'test',
+    with: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    }
+};
+
